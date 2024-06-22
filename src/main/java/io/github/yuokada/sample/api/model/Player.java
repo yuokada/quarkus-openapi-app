3282e14ebcf90c900ba96a84dad5229762486446
Yukihiro Okada <yuokada@users.noreply.github.com>
2024-06-22 16:19:45 +0900

    Add annotations

--- src/main/java/io/github/yuokada/sample/api/model/Player.java
+++ src/main/java/io/github/yuokada/sample/api/model/Player.java
@@ -1,5 +1,20 @@
 package io.github.yuokada.sample.api.model;
 
-public record Player(Long userId, String name, Integer age) {
+import com.fasterxml.jackson.annotation.JsonProperty;
+import org.eclipse.microprofile.openapi.annotations.enums.SchemaType;
+import org.eclipse.microprofile.openapi.annotations.media.Schema;
+
+@Schema(name = "Player json overview")
+public record Player(
+    @JsonProperty("id")
+    @Schema(description = "The user Id", type = SchemaType.NUMBER, format = "int64")
+    Long userId,
+    @Schema(minLength = 4, maxLength = 64)
+    String name,
+    @Schema(description = "The user's age", type = SchemaType.NUMBER, format = "short",
+        maximum = "150", minimum = "20"
+    )
+    Integer age
+) {
 
 }
